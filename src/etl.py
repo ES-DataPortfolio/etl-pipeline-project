c60405c08970b507ca2289d8b610fa3c0b394896
ES-DataPortfolio <eliaswstockhausen@gmail.com>
2025-08-08 00:24:18 +0200

    Update ETL script with integration of helper modules

--- src/etl.py
+++ src/etl.py
@@ -1,20 +1,21 @@
-import pandas as pd
 from pathlib import Path
+from extract import extract_data
+from transform import clean_data
+from validate import validate_data
+from load import save_data
 
 # 1. Paths
-RAW_DATA = Path(r"C:\Users\User\Dev\etl_pipeline_project\data\raw\zara.csv")
-PROCESSED_DATA = Path(r"C:\Users\User\Dev\etl_pipeline_project\data\processed\zara_processed.csv")
+RAW_PATH = Path(r"C:\Users\User\Dev\etl_pipeline_project\data\raw\zara.csv")
+PROCESSED_PATH = Path(r"C:\Users\User\Dev\etl_pipeline_project\data\processed\zara_processed.csv")
 
-# 2. Extract
-print("Loading raw data...")
-df = pd.read_csv(RAW_DATA)
 
-# 3. Transform (mini-cleaning)
-print("Cleaning data...")
-df.columns = df.columns.str.strip().str.lower().str.replace(" ", "_")
+def run_etl():
+    df = extract_data(RAW_PATH)
+    df_transform = clean_data(df)
+    print(df.columns.tolist())
+    validate_data(df_transform)
+    save_data(df_transform, PROCESSED_PATH)
+    print("ETL pipeline completed successfully!")
 
-# 4. Load
-print("Saving cleaned data...")
-df.to_csv(PROCESSED_DATA, index=False)
-
-print("ETL pipeline finished successfully!")
+if __name__ == "__main__":
+    run_etl()
